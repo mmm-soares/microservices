16b72b1c5cbbf732120a161413825ab3290f8345
mmm-soares <60071707+mmm-soares@users.noreply.github.com>
2020-05-10 16:15:37 +0100

    Add JPA repository

---
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/BeerRepository.java
@@ -0,0 +1,14 @@
+package guru.springframework.msscbeerservice;
+
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
+
+}
---
+++ mssc-beer-service/src/main/java/guru/springframework/msscbeerservice/repositories.java
@@ -0,0 +1,4 @@
+package guru.springframework.msscbeerservice;
+
+public class repositories {
+}
