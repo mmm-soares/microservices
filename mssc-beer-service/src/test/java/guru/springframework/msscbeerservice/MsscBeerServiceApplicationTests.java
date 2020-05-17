99a85ed2ace7550d7588a52527c24d0ae321b40e
Márcio Soares <msoares8000@gmail.com>
2020-05-17 19:01:24 +0100

    Deconstructing the monolith #10
    - add encache

--- mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ mssc-beer-service/src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,13 +1,14 @@
 package guru.springframework.msscbeerservice;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MsscBeerServiceApplicationTests {
+public class MsscBeerServiceApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
